2c6739cd83ffd99a03b7192c0ed5009e0f9f3347
Ivy B <80648381+ivybergstrom@users.noreply.github.com>
2021-10-07 08:53:19 -0600

    Phase 3 completed with no issue

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const overview = document.querySelector(".overview"); //this is the div where profile info will appear
 const username = "ivybergstrom"; //my github username
-
+const displayReposList = document.querySelector(".repo-list") //ul to display repos 
 
 //function to get info from my github profile
 const getBergi = async function () {
@@ -27,4 +27,22 @@ const displayUserInfo = function (data) {
             <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
         </div>`;
     overview.append(div);
+    getRepos();
 }
+
+const getRepos = async function () {
+    const reposCollect = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const reposData = await reposCollect.json();
+    displayRepoInfo(reposData);
+};
+
+//the function below is for displaying repo information that is pulled by the getRepos function
+const displayRepoInfo = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3> ${repo.name} </h3>`;
+        displayReposList.append(repoItem);
+    }
+    
+};
